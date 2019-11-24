53ba5db690681f6294d290583210ca652ab08dd7
alvaro <royerdcq@gmail.com>
2019-11-24 14:42:56 -0500

    serverless

---
+++ src/java/Coneccion/coneccion.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Coneccion;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+/**
+ *
+ * @author hp
+ */
+public class coneccion {
+    
+    public static String url = "jdbc:mysql://localhost/pruebita";
+    public static String user = "root";
+    public static String paswd = "12345678";
+    public static Connection cn;
+    public static Connection Abrir() {
+        
+          
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            cn = DriverManager.getConnection(url, user, paswd);
+            return cn;
+        } catch (Exception e) {
+            return null;
+        }
+        
+    }
+    
+    
+}
